bfd6d63780b357aa7d59caa212a3d6a6bd0b2f06
oliburt <oliburt@gmail.com>
2019-08-28 15:38:11 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,49 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do
+    redirect to '/recipes'
+  end
 
+  # INDEX
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  # NEW
+  get '/recipes/new' do
+    erb :new
+  end
+
+  # SHOW
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  # CREATE
+  post '/recipes' do
+    params[:recipe] ? recipe = Recipe.create(params[:recipe]) : recipe = Recipe.create(params)
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  # EDIT
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  # UPDATE
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    recipe.update(params[:recipe])
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  # DESTROY
+  delete '/recipes/:id' do
+    Recipe.destroy(params[:id])
+    redirect to '/recipes'
+  end
 end
---
+++ db/migrate/20190828122201_add_cook_time_to_recipes.rb
@@ -0,0 +1,5 @@
+class AddCookTimeToRecipes < ActiveRecord::Migration
+  def change
+    add_column :recipes, :cook_time, :integer
+  end
+end
